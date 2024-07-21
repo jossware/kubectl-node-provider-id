404a7438a84f7a8cfbaf8b75c71b994e9d06bc1d
ryane <ryanesc@gmail.com>
2024-07-21 11:42:52 -0400

    add config file support
    so you can choose default template and / or output format

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod cli;
 mod print;
 
 use clap::Parser;
-use cli::{Cli, OutputFormat};
+use cli::{Cli, OutputConfig, OutputFormat};
+use color_eyre::eyre::Context;
+use etcetera::{choose_base_strategy, BaseStrategy};
 use k8s_openapi::api::core::v1::Node;
 use kube::{Api, ResourceExt};
 use node_provider_labeler::{provider_id::ProviderID, template::Template};
 use serde::Serialize;
-use std::str::FromStr;
+use std::{path::Path, str::FromStr};
 
 fn init_tracing() {
     std::env::set_var(
@@ -27,7 +29,26 @@ async fn main() -> color_eyre::Result<()> {
 }
 
 async fn run() -> color_eyre::Result<()> {
+    let base_strategy = choose_base_strategy().wrap_err("discovering configuration directory")?;
+    let config_file = Path::new(&base_strategy.config_dir()).join("kubectl-node-provider-id.yaml");
+    let output_config = {
+        if config_file.exists() {
+            let data =
+                std::fs::read_to_string(config_file).wrap_err("reading configuration file")?;
+            serde_yaml::from_str(&data).wrap_err("loading configuration from file")?
+        } else {
+            OutputConfig::default()
+        }
+    };
+
     let cli = Cli::parse();
+
+    let template = cli
+        .template
+        .unwrap_or(output_config.template.unwrap_or_default());
+    let output_format = cli
+        .output_format
+        .unwrap_or(output_config.format.unwrap_or_default());
 
     let client_config = match cli.context {
         Some(ref context) => {
@@ -40,12 +61,14 @@ async fn run() -> color_eyre::Result<()> {
         None => kube::Config::infer().await?,
     };
 
-    let template = &cli.template;
-    let nodes =
-        get_nodes_with_provider_id(kube::Client::try_from(client_config)?, cli.name, template)
-            .await?;
+    let nodes = get_nodes_with_provider_id(
+        kube::Client::try_from(client_config)?,
+        cli.name,
+        &template.to_string(),
+    )
+    .await?;
 
-    match cli.output_format {
+    match output_format {
         OutputFormat::Plain => print::plain(nodes)?,
         OutputFormat::Json => print::json(nodes)?,
         OutputFormat::Yaml => print::yaml(nodes)?,
