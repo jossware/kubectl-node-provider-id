4870e95ea82bac16f8a686585b32ada4bdaf8dcf
ryane <ryanesc@gmail.com>
2024-08-01 06:14:50 -0400

    remove krew integration
    moved to https://github.com/jossware/krew-index

--- flake.nix
+++ flake.nix
@@ -26,38 +26,6 @@
         pkgs = import nixpkgs { inherit system overlays; };
         inherit (pkgs) lib;
 
-        krew-release-bot = pkgs.buildGoModule rec {
-          pname = "krew-release-bot";
-          version = "0.0.46";
-
-          src = pkgs.fetchFromGitHub {
-            rev = "v${version}";
-            owner = "rajatjindal";
-            repo = "krew-release-bot";
-            sha256 = "sha256-73r4kT+J5DeAx0g5RcLBIJwBrXfmRwjRXFoEhmEVu/M=";
-          };
-
-          vendorHash = null;
-
-          nativeBuildInputs = [ pkgs.installShellFiles ];
-
-          subPackages = [ "cmd/action" ];
-
-          CGO_ENABLED = 0;
-
-          ldflags = [
-            "-s"
-            "-w"
-          ];
-
-          # Rename the output binary
-          postInstall = ''
-            mv $out/bin/action $out/bin/krew-release-bot
-          '';
-
-          doCheck = false;
-        };
-
         src = lib.cleanSourceWith { src = ./.; };
         nativeBuildInputs =
           with pkgs;
@@ -74,7 +42,6 @@
           buildInputs = with pkgs; [
             kubectl
             krew
-            krew-release-bot
             rust-bin.stable.latest.default
             rust-analyzer
           ];
